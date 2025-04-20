976968125ef656f7ccee53fa9f38f277d379b161
vinith-0430 <vinithmohan2006@gmail.com>
2025-04-20 20:09:02 +0400

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+// firebase.js
+    const firebaseConfig = {
+        apiKey: "AIzaSyDWxPCS4MyyTsVfAt22EpQoWtHTMb9KjKY",
+        authDomain: "tuluproject-718bc.firebaseapp.com",
+        projectId: "tuluproject-718bc",
+        storageBucket: "tuluproject-718bc.firebasestorage.app",
+        messagingSenderId: "150946663776",
+        appId: "1:150946663776:web:f54be8151e119cc0ea2205",
+        measurementId: "G-G6N26K43WB"
+      };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  
